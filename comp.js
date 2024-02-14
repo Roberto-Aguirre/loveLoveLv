c622af4e937ee5f931bf03b4800dc1f3540a522a
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-02-13 22:36:17 -0600

    Cambios realizados al landing page y funcion que inserta las fotos

--- comp.js
+++ comp.js
@@ -1,4 +1,16 @@
-const lista = document.querySelector("ul");
+const fotosPanel = document.getElementById("panelFotos");
+const objetoImagen = document.createElement("li");
+const imagen = document.createElement("img");
 
-lista.append('<h1>Hola<h1>')
+function mostrarImagenes(){
+for (let i = 1; i<=195 ; i++) {
+    const imagen = document.createElement("img");
+    imagen.setAttribute("src",`frida/2 (${i}).jpg`)
+    imagen.setAttribute("class","imagenes-cuadros");
+    document.body.append(imagen)
+}
 
+}
+mostrarImagenes()
+
+console.log(imagen);
